bb418157543a5e4df6e877e41680996d05bb8f15
TamCore <philipp@tamcore.eu>
2014-11-24 19:01:14 +0100

    make sure each file is unique, and save submitter's address..

--- post.php
+++ post.php
@@ -1,7 +1,12 @@
 <?php
     if (isset($_POST['code'], $_POST['submit'])) {
-        $cryptid = $crypt = substr(md5(microtime()), 0, 10);
-        file_put_contents('pastes/'.$crypt.'.txt', $_POST['code']);
+        $cryptid = '';
+        while ($cryptid == '') {
+	    $cryptid = dechex(time() . rand(0, 99));
+            if (file_exists('pastes/'.$cryptid.'.txt')) $cryptid = '';
+        }
+        file_put_contents('pastes/'.$cryptid.'.txt', $_POST['code']);
+        file_put_contents('pastes/'.$cryptid.'.meta', 'Creator: ' . $_SERVER['REMOTE_ADDR'] . "\n");
         $url = "http://".$_SERVER['HTTP_HOST']."/".$cryptid.".txt";
         echo $url;
         header("Location: ".$url, true, 301);
