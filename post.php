000b5b54b9c5b6c3d6870c21d27cc76f5d52ff66
TamCore <philipp@tamcore.eu>
2014-11-29 23:46:29 +0100

    allow posting via 'cat filename | curl --data-binary @- http://paste.tamcore.eu/post.php'

--- post.php
+++ post.php
@@ -1,16 +1,20 @@
 <?php
     if (isset($_POST['code'], $_POST['submit'])) {
-        $cryptid = '';
-        while ($cryptid == '') {
-	    $cryptid = dechex(time() . rand(0, 99));
-            if (file_exists('pastes/'.$cryptid.'.txt')) $cryptid = '';
-        }
-        file_put_contents('pastes/'.$cryptid.'.txt', $_POST['code']);
-        file_put_contents('pastes/'.$cryptid.'.meta', 'Creator: ' . $_SERVER['REMOTE_ADDR'] . "\n");
-        $url = "http://".$_SERVER['HTTP_HOST']."/".$cryptid.".txt";
-        echo $url;
-        header("Location: ".$url, true, 301);
+      $code = $_POST['code'];
+    } elseif (isset($_SERVER['CONTENT_LENGTH'])) {
+      $code = file_get_contents('php://input');
     } else {
         require("index.php");
+        exit;
     }
+    $cryptid = '';
+    while ($cryptid == '') {
+        $cryptid = dechex(time() . rand(0, 99));
+        if (file_exists('pastes/'.$cryptid.'.txt')) $cryptid = '';
+    }
+    file_put_contents('pastes/'.$cryptid.'.txt', $code);
+    file_put_contents('pastes/'.$cryptid.'.meta', 'Creator: ' . $_SERVER['REMOTE_ADDR'] . "\n");
+    $url = "http://".$_SERVER['HTTP_HOST']."/".$cryptid.".txt";
+    echo $url;
+    header("Location: ".$url, true, 301);
 ?>
